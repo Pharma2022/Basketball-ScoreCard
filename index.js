60c51c8dae1b458233526aa55878797471aa4fb4
Pharma2022 <111680056+Pharma2022@users.noreply.github.com>
2022-12-18 06:13:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,39 @@
-let homeScoreEl=document.getElementById("home-el")
-let homeScore=0
 
-let guestScoreEl=document.getElementById("guest-el")
+let homeScore=0
 let guestScore=0
 
+document.getElementById("homeBtn1").addEventListener("click",()=>homeAdd(1))
+document.getElementById("homeBtn2").addEventListener("click",()=>homeAdd(2))
+document.getElementById("homeBtn3").addEventListener("click",()=>homeAdd(3))
+document.getElementById("GuestBtn1").addEventListener("click",()=>guestAdd(1))
+document.getElementById("GuestBtn2").addEventListener("click",()=>guestAdd(2))
+document.getElementById("GuestBtn3").addEventListener("click",()=>guestAdd(3))
+document.getElementById("reset").addEventListener("click", reset)
 
+function reset(){
+    guestScore=0
+    document.getElementById("guest-el").textContent=guestScore
+    homeScore=0
+    document.getElementById("home-el").textContent=homeScore
+}
 
 function homeAdd(x){
     homeScore+=x
-     homeScoreEl.textContent=homeScore
+     document.getElementById("home-el").textContent=homeScore
 }
 
-document.getElementById("homeBtn1").addEventListener("click",function(){
-    homeAdd(1)
-})
-document.getElementById("homeBtn2").addEventListener("click",function(){
-    homeAdd(2)
-})
-document.getElementById("homeBtn3").addEventListener("click",function(){
-    homeAdd(3)
-})
 
 
 function guestAdd(x)
 { guestScore+=x
-guestScoreEl.textContent=guestScore}
+document.getElementById("guest-el").textContent=guestScore}
 
-document.getElementById("GuestBtn1").addEventListener("click",function(){
-    guestAdd(1)
-})
-document.getElementById("GuestBtn2").addEventListener("click",function(){
-    guestAdd(2)
-})
-
-document.getElementById("GuestBtn3").addEventListener("click",function(){
-    guestAdd(3)
-})
-
-function reset(){
-    guestScore=0
-    guestScoreEl.textContent=guestScore
-    homeScore=0
-    homeScoreEl.textContent=homeScore
-}
-
-
-document.getElementById("btn").addEventListener("click",function gfgMenu() 
-            {const nav = document.querySelector('.nav');
+document.getElementById("btn").addEventListener("click",()=>{ 
+            const nav = document.querySelector('.nav');
   
-            if (nav.style.display === "none") 
-            {nav.style.display = "block";}
-            else {nav.style.display = "none";}
-              }
+            nav.style.display === "none"? 
+            nav.style.display = "block"
+            : nav.style.display = "none"
+}
         )
 
