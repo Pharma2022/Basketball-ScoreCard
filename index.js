1e28e6f8381f78091900131d5d658b9867ff976b
Pharma2022 <111680056+Pharma2022@users.noreply.github.com>
2022-12-18 22:00:13 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 
 let homeScore=0
 let guestScore=0
+document.addEventListener("click", e =>{
+    e.preventDefault()
+    e.target.dataset.home? homeAdd(JSON.parse(e.target.dataset.home)):""
+    e.target.dataset.guest? guestAdd(JSON.parse(e.target.dataset.guest)):""
+    e.target.id=== "reset"? reset():""  })
 
-document.getElementById("homeBtn1").addEventListener("click",()=>homeAdd(1))
-document.getElementById("homeBtn2").addEventListener("click",()=>homeAdd(2))
-document.getElementById("homeBtn3").addEventListener("click",()=>homeAdd(3))
-document.getElementById("GuestBtn1").addEventListener("click",()=>guestAdd(1))
-document.getElementById("GuestBtn2").addEventListener("click",()=>guestAdd(2))
-document.getElementById("GuestBtn3").addEventListener("click",()=>guestAdd(3))
-document.getElementById("reset").addEventListener("click", reset)
 
+function renderScore(){
+    document.getElementById("guest-el").textContent=guestScore
+    document.getElementById("home-el").textContent=homeScore}
 function reset(){
     guestScore=0
-    document.getElementById("guest-el").textContent=guestScore
     homeScore=0
-    document.getElementById("home-el").textContent=homeScore
-}
+    renderScore()}
 
 function homeAdd(x){
-    homeScore+=x
-     document.getElementById("home-el").textContent=homeScore
-}
-
+homeScore+=x
+renderScore()}
 
 
 function guestAdd(x)
 { guestScore+=x
-document.getElementById("guest-el").textContent=guestScore}
+renderScore()}
 
 document.getElementById("btn").addEventListener("click",()=>{ 
             const nav = document.querySelector('.nav');
   
             nav.style.display === "none"? 
             nav.style.display = "block"
-            : nav.style.display = "none"
-}
-        )
+            : nav.style.display = "none"})
 
