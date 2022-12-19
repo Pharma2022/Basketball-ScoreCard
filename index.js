f667440d1407a5dec7f3661824e7cc7fcbf46fdb
Pharma2022 <111680056+Pharma2022@users.noreply.github.com>
2022-12-19 21:25:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-
 let homeScore=0
 let guestScore=0
+
 document.addEventListener("click", e =>{
     e.preventDefault()
     e.target.dataset.home? homeAdd(JSON.parse(e.target.dataset.home)):""
     e.target.dataset.guest? guestAdd(JSON.parse(e.target.dataset.guest)):""
     e.target.id=== "reset"? reset():""  })
 
-
 function renderScore(){
     document.getElementById("guest-el").textContent=guestScore
     document.getElementById("home-el").textContent=homeScore}
+
 function reset(){
     guestScore=0
     homeScore=0
@@ -20,15 +20,9 @@ function homeAdd(x){
 homeScore+=x
 renderScore()}
 
-
 function guestAdd(x)
 { guestScore+=x
 renderScore()}
 
-document.getElementById("btn").addEventListener("click",()=>{ 
-            const nav = document.querySelector('.nav');
-  
-            nav.style.display === "none"? 
-            nav.style.display = "block"
-            : nav.style.display = "none"})
+
 
